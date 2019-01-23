51703de811db4e005d5e1e04ddb7e99049f0aa30
ryanalesna <45490322+ryanalesna@users.noreply.github.com>
2019-01-23 14:29:34 -0500

    testing to see if commit works with object factory stuff

---
+++ CppWorkspace/Object_Factory.cpp
@@ -0,0 +1,6 @@
+#include "Object_Factory.h"
+
+Dictionary<GenericVehicleFactory*> g_mVehicleFactories;
+
+LINK_CLASS_TO_NAME(Car, "car");
+//LINK_CLASS_TO_NAME(SemiTruck, "semi-truck");
---
+++ CppWorkspace/Object_Factory.h
@@ -0,0 +1,86 @@
+//Implement CreateVehicleByName Function using correct Dictionary 
+//Change constructor in vehicle to set name and set type 
+
+#ifndef OBJECT_FACTORY_H
+#define OBJECT_FACTORY_H
+#define LINK_CLASS_TO_NAME(Class, Name) SpecificVehicleFactory<Class> g_##Class##Factory(#Name);  
+
+#include "Dictionary.h"
+class GenericVehicleFactory;
+extern Dictionary<GenericVehicleFactory*> g_mVehicleFactories;
+class Vehicle;
+
+class GenericVehicleFactory
+{
+public:
+	GenericVehicleFactory(const char* type)
+	{
+		const char* object_Type = type;
+		g_mVehicleFactories.add(type, this);
+	}
+
+	virtual Vehicle* create(const char* type, const char* name)
+	{
+		return NULL;
+	}
+};
+
+template <class T>
+class SpecificVehicleFactory : GenericVehicleFactory
+{
+public:
+
+	SpecificVehicleFactory(const char* type) : GenericVehicleFactory(type) {}
+
+	const char* object_Type;
+	Vehicle* create(const char* type, const char* name)
+	{
+		return new T(type, name);
+	}
+};
+
+class Vehicle
+{
+	Vehicle() { };
+
+private:
+	const char* m_type;
+	const char* m_name;
+public:
+	const char* setType(const char* type)
+	{
+		type = m_type;
+	}
+	const char* setName(const char* name)
+	{
+		name = m_name;
+	}
+	virtual void printNameOfVehicle()
+	{
+		printf(m_name);
+	}
+	void printTypeOfVehicle()
+	{
+		printf(m_type);
+	}
+};
+
+class Car : public Vehicle
+{ 
+	//Car() : Vehicle() {}
+
+};
+class SemiTruck : public Vehicle
+{ };
+Vehicle* createVehicleByName(const char* pszTypeName, const char* pszInstanceName)
+{
+	GenericVehicleFactory* pFactory = g_mVehicleFactories.lookup(pszTypeName);
+	Vehicle* pVehicle = pFactory->create(pszTypeName, pszInstanceName);
+	return pVehicle;
+}
+
+
+
+
+
+#endif
